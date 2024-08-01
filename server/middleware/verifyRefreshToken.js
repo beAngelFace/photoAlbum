16237b33e2c23bcd5ae0a6dbf2dca166b69f7416
beAngelFace <be.angelface@gmail.com>
2024-08-01 12:27:22 +0300

    dinner

--- server/middleware/verifyRefreshToken.js
+++ server/middleware/verifyRefreshToken.js
@@ -5,6 +5,8 @@ function verifyRefreshToken(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
       const { user } = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+      //отличие от аксес токена в том, что рефреш мы забираем из заголовка
   
       res.locals.user = user;
   
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,38 @@
 const router = require('express').Router();
+const {User} = require('../db/models');
+const bcrypt = require('bcrypt');
+const generateTokens = require('../utils/generateTokens');
+const jwtConfig = require('../config/jwtConfig')
 
-router.post('/registration', (req, res) => {
+router.post('/registration', async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        if(name.trim() === '' ||
+        email.trim() === '' ||
+        password.trim() === '') {
+            return res.status(400).json({message: 'Заполните все поля'})
+        }
+        const userInDb = await User.findOne({where: {email}})
+        if (userInDb) {
+            return res.status(400).json({message: 'Такой пользователь уже существует'})
+        } else {
+            const user = (await User.create({name, email, password: await bcrypt.hash(password, 10)})).get();
+
+            const {accessToken, refreshToken} = generateTokens({user});
+            console.log(user);
+
+
+            res
+            .status(201)
+            .cookie(jwtConfig.refresh.type, refreshToken, {httpOnly: true, maxAge: jwtConfig.refresh.expiresIn})
+            .json({accessToken, user})
+        }
+        
+    } catch (error) {
+        res.status(400).json(error: error.message)
+    }
+
 
 });
 
