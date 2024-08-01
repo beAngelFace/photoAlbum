f48406fa9cc521e5e01b84226ca73e132db689a5
beAngelFace <be.angelface@gmail.com>
2024-08-01 22:20:58 +0300

    routes done

--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,9 +1,12 @@
 const router = require ('express').Router()
 const tokensRouter = require('./tokens.routes')
 const authRouter = require('./auth.routes')
+const albumsRouter = require('./albums.routes')
+const photosRouter = require('./photo.routes')
 
-
+router.use('/photo', photosRouter);
 router.use('/auth', authRouter);
 router.use('/tokens', tokensRouter);
+router.use('/albums', albumsRouter)
 
 module.exports = router;
