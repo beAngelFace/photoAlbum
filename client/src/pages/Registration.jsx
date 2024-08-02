a2508d600c3481dae27a55f983c675040a9c7658
unknown <benya0192@mail.ru>
2024-08-02 11:50:53 +0300

    lg

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,42 +1,65 @@
 import { useState } from "react";
-import AxiosInstance, { setAccessToken } from "../service/AxiosInstance";
+import axiosInstance, { setAccessToken } from "../service/axiosInstance";
+import { useNavigate } from "react-router-dom";
 
-function RegistrationPage({setUser}) {
+function RegistrationPage({ setUser }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const navigate = useNavigate()
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirm, setConfirm] = useState('')
+  const registrationUser = (event) => {
+    event.preventDefault();
 
-    const registrationUser = (event) => {
-        event.preventDefault();
+    if (confirm === password) {
+      axiosInstance
+        .post("/auth/registration", { name, email, password })
+        .then(({ data }) => {
+          console.log(data);
+          setAccessToken(data.accessToken);
+          setUser(data.user);
+          navigate('/');
+         
+        })
 
-        if( confirm === password) {
-            AxiosInstance.post('/auth/registration', {name, email, password})
-                .then(({data}) => {
-                    console.log(data);
-                    setAccessToken(data.accessToken)
-                    setUser(data.user)
-                })
-               
-                .catch(err => console.log(err))
-        }
+        .catch((err) => console.log(err));
     }
+  };
 
-    return (
-        <>
-            <h1>Registration</h1>
+  return (
+    <>
+      <h1>Registration</h1>
 
-            <form onSubmit={registrationUser}>
-                <input type="text" onChange={({target}) => setName(target.value)} placeholder="Name" required/>
-                <input type="text" onChange={({target}) => setEmail(target.value)} placeholder="Email" required />
-                <input type="text" onChange={({target}) => setPassword(target.value)} placeholder="Password" required />
-                <input type="text" onChange={({target}) => setConfirm(target.value)} placeholder="Confirm password" required />
-                <button type="submit">Registration</button>
-            </form>
-        </>
-        
-    );
+      <form onSubmit={registrationUser}>
+        <input
+          type="text"
+          onChange={({ target }) => setName(target.value)}
+          placeholder="Name"
+          required
+        />
+        <input
+          type="text"
+          onChange={({ target }) => setEmail(target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          onChange={({ target }) => setPassword(target.value)}
+          placeholder="Password"
+          required
+        />
+        <input
+          type="password"
+          onChange={({ target }) => setConfirm(target.value)}
+          placeholder="Confirm password"
+          required
+        />
+        <button type="submit">Registration</button>
+      </form>
+    </>
+  );
 }
 
 export default RegistrationPage;
